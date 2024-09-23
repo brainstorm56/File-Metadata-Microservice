40c9ce646ffc4ea02d986dcff5e067f96428020d
Abhinav Gupta <abhinav6483@gmail.com>
2024-09-23 13:34:11 +0530

    seems complete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import multer from 'multer'
+dotenv.config();
 
 var app = express();
 
@@ -11,6 +13,30 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Define where to store the files and their naming convention
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Specify the folder to store files
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    // Set the filename (original name in this case)
+    cb(null, file.originalname);
+  }
+});
+
+// Set up the Multer instance
+const upload = multer({ storage: storage });
+
+app.post('/api/fileanalyse', upload.single('upfile'),(req, res) => {
+  const file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  }
+  );
+});
 
 
 
